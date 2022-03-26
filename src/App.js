1150b862118508f664df4f95875d88fdf6f19120
sarirkarim <sarirkarim75@gmail.com>
2022-03-26 21:25:17 +0500

    authentication complete

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import { useState, useEffect } from "react";
 import "./App.css";
 import Post from "./Post";
-import { db,auth } from "./firebase";
+import { db, auth } from "./firebase";
 import Modal from "@material-ui/core/Modal";
 import { makeStyles } from "@material-ui/core/styles";
 import { Button, Input } from "@material-ui/core";
 
 function getModelStyle() {
   const top = 50;
-  const left = 50;
+  const left = 10;
 
   return {
     top: `${top}%`,
@@ -34,11 +34,29 @@ function App() {
 
   const [posts, setPosts] = useState([]);
   const [open, setOpen] = useState(false);
+  const [openSignIn, setOpenSignIn] = useState(false);
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
   const [email, setEmail] = useState("");
-  
+  const [user, setUser] = useState(null);
 
+  useEffect(() => {
+    const unsubscribe = auth.onAuthStateChanged((authUser) => {
+      if (authUser) {
+        //user has logedin
+        console.log(authUser);
+        setUser(authUser);
+      } else {
+        //user has logged out
+        setUser(null);
+      }
+    });
+
+    return () => {
+      //perform some cleanup actions
+      unsubscribe();
+    };
+  }, [user, username]);
   // useEffect -> runs a piece of code in a specific condition
   useEffect(() => {
     //this is where the code runs
@@ -54,9 +72,24 @@ function App() {
   }, []);
 
   const signUp = (e) => {
-    e.preventDefault()
-    auth.createUserWithEmailAndPassword(email, password)
-    .catch((error) => alert(error.message))
+    e.preventDefault();
+    auth
+      .createUserWithEmailAndPassword(email, password)
+      .then((authUser) => {
+        return authUser.user.updateProfile({
+          displayName: username,
+        });
+      })
+      .catch((error) => alert(error.message));
+    setOpen(false);
+  };
+
+  const signIn = (e) => {
+    e.preventDefault();
+    auth
+      .signInWithEmailAndPassword(email, password)
+      .catch((error) => error.message);
+    setOpenSignIn(false);
   };
   return (
     <div className="App">
@@ -94,12 +127,56 @@ function App() {
               onChange={(e) => setPassword(e.target.value)}
             />
 
-            <Button type="submit" onClick={signUp}>SignUp</Button>
+            <Button type="submit" onClick={signUp}>
+              SignUp
+            </Button>
           </form>
         </div>
       </Modal>
 
-      <Button onClick={() => setOpen(true)}>SignUP</Button>
+      <Modal
+        open={openSignIn}
+        onClose={() => {
+          setOpenSignIn(false);
+        }}
+      >
+        <div style={modalStyle} className={classes.paper}>
+          <form className="app__signup">
+            <center>
+              <img
+                className="app__headerImage"
+                src="https://www.instagram.com/static/images/web/mobile_nav_type_logo.png/735145cfe0a4.png"
+                alt="instagram logo"
+              />
+            </center>
+            <Input
+              placeholder="email"
+              type="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+            <Input
+              placeholder="password"
+              type="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+
+            <Button type="submit" onClick={signIn}>
+              SignIn
+            </Button>
+          </form>
+        </div>
+      </Modal>
+
+      {user ? (
+        <Button onClick={() => auth.signOut()}>LogOut</Button>
+      ) : (
+        <div className="app__loginContainer">
+          <Button onClick={() => setOpenSignIn(true)}>SignIN</Button>
+          <Button onClick={() => setOpen(true)}>SignUP</Button>
+        </div>
+      )}
 
       {posts.map(({ id, post }) => (
         <Post
